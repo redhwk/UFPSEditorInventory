abab8967d1610b7caecd56200f376dbd7f3e3999
redhwk <chris.redhawk@gmail.com>
2016-02-18 12:48:36 -0500

    Create WeaponItemDefaultsList.cs

---
+++ RedHawk/ScriptableObject/WeaponItemDefaultsList.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RedHawk{
+	public class WeaponItemDefaultsList : ScriptableObject {
+		public List<WeaponItemDefaults> weaponItems=new List<WeaponItemDefaults>();
+
+	}
+}

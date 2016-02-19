8f7acd36a313512e9b25dd0d001fe94ff6e901ce
redhwk <chris.redhawk@gmail.com>
2016-02-19 13:11:56 -0500

    Added files via upload

---
+++ RedHawk/Example/RocketProjectile/ModGrenade.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RedHawk{
+	[RequireComponent(typeof(AudioSource))]
+	public class ModGrenade : vp_Grenade {
+		private vp_Timer.Handle Timer = new vp_Timer.Handle();
+		void OnDestroy(){
+			Timer.Cancel();
+		}
+		void Start () {
+			vp_Timer.In(LifeTime, delegate()
+				{
+					transform.SendMessage("DieBySources", new Transform[]{m_Source, m_OriginalSource}, SendMessageOptions.DontRequireReceiver);
+				},Timer);
+		}
+	}
+}
